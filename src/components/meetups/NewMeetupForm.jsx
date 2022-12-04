4f3dbfc62818e919c675e00ca742fab5e1eeaaf5
Stav <stavush93@gmail.com>
2022-12-04 19:45:47 +0200

    adding a New Meetup Form

---
+++ src/components/meetups/NewMeetupForm.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Card from "../UI/Card";
+import classes from "./NewMeetupForm.module.css";
+
+function NewMeetupForm() {
+  return (
+    <Card>
+      <form className={classes.form}>
+        <div className={classes.control}>
+          <label htmlFor="title">Meetup Title</label>
+          <input type="text" required id="title" />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="image">Meetup Image URL</label>
+          <input type="url" required id="image" />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="address">Meetup Address</label>
+          <input type="text" required id="address" />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="description">Meetup Description</label>
+          <textarea type="text" required rows="5" id="description"></textarea>
+        </div>
+        <div className={classes.actions}>
+          <button>
+           Add Meetup
+          </button>
+        </div>
+      </form>
+    </Card>
+  );
+}
+
+export default NewMeetupForm;
