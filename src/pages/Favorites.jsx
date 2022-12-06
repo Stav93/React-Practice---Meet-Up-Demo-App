12971236ffdbff6e89de236c25540f857ee087bb
Stav <stavush93@gmail.com>
2022-12-06 20:50:06 +0200

    show favorites in favorites page

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,5 +1,24 @@
+import { useContext } from "react";
+import FavoritesContext from "../context/favorites-context"
+import MeetupList from "../components/meetups/MeetupList"
+
 function FavoritesPage() {
-  return <div>Favorites Page</div>
+  const favortietCtx = useContext(FavoritesContext)
+
+  let content;
+
+  if (favortietCtx.totalFavorites === 0) {
+    content = <p>No Favorites Yet</p>
+  } else {
+    content = <MeetupList meetups={favortietCtx.favorites} />
+  }
+
+  return (
+    <section>
+      <h1>My Favorites</h1>
+      {content}
+    </section>
+  )
 }
 
 export default FavoritesPage;
